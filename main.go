fd5b636920a1a084c0094a407d188bb8bd419b39
Pieter Noordhuis <pcnoordhuis@gmail.com>
2020-03-28 20:12:24 +0100

    Recreate connection to PDU for every command

--- main.go
+++ main.go
@@ -3,12 +3,34 @@ package main
 import (
 	"flag"
 	"log"
-	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/pietern/pductl/pdu"
 	"github.com/pietern/pductl/watchdog"
 )
+
+type wrapper struct {
+	c Configuration
+}
+
+func (w *wrapper) Run(args ...string) error {
+	pdu, err := pdu.Dial("tcp", w.c.PDU.Address, w.c.PDU.Timeout.Duration)
+	if err != nil {
+		return err
+	}
+
+	err = pdu.Authenticate(w.c.PDU.Username, w.c.PDU.Password)
+	if err != nil {
+		return err
+	}
+
+	_, err = pdu.Run(args...)
+	if err != nil {
+		return err
+	}
+
+	return pdu.Logout()
+}
 
 func main() {
 	var path string
@@ -21,15 +43,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	pdu, err := pdu.Dial("tcp", c.PDU.Address, c.PDU.Timeout.Duration)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = pdu.Authenticate(c.PDU.Username, c.PDU.Password)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Convenience wrapper to connect, authenticate, run a
+	// command, and logout again.
+	//
+	// It proves difficult to keep a single connection alive for a
+	// longer period of time, so instead we just recreate a
+	// connection every time we need to perform some action.
+	//
+	w := &wrapper{c}
 
 	// Create aggregate channel for tuples containing both the
 	// presence or absence signal and the outlet information.
@@ -52,27 +73,24 @@ func main() {
 		}(monitor, outlet)
 	}
 
+	// Run something to see if the credentials are valid.
+	err = w.Run("whoami")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Main loop.
-	keepalive := time.NewTicker(c.PDU.Timeout.Duration)
-	for {
-		select {
-		case t := <-ch:
-			switch t.State {
-			case watchdog.Present:
-				log.Printf("Turning %v on\n", t.Outlet.Name)
-				_, err := pdu.Run("on", t.Outlet.Name)
-				if err != nil {
-					log.Fatal(err)
-				}
-			case watchdog.Absent:
-				log.Printf("Turning %v off\n", t.Outlet.Name)
-				_, err := pdu.Run("off", t.Outlet.Name)
-				if err != nil {
-					log.Fatal(err)
-				}
+	for t := range ch {
+		switch t.State {
+		case watchdog.Present:
+			log.Printf("Turning %v on\n", t.Outlet.Name)
+			err := w.Run("on", t.Outlet.Name)
+			if err != nil {
+				log.Fatal(err)
 			}
-		case <-keepalive.C:
-			_, err := pdu.Whoami()
+		case watchdog.Absent:
+			log.Printf("Turning %v off\n", t.Outlet.Name)
+			err := w.Run("off", t.Outlet.Name)
 			if err != nil {
 				log.Fatal(err)
 			}
